008db128a2bfbacb784f810be2f7ec1b26dc5802
frist commit <>
2018-08-27 11:02:30 +0800

    MQTTDemo 配置工程，第一次提交

---
+++ example/MQTTDemo/MQTTDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MQTTDemo
+//
+//  Created by 肖旭晖 on 2018/8/27.
+//  Copyright © 2018年 [简书](https://www.jianshu.com/p/e1d8b4eb0ecd). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ example/MQTTDemo/MQTTDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  MQTTDemo
+//
+//  Created by 肖旭晖 on 2018/8/27.
+//  Copyright © 2018年 [简书](https://www.jianshu.com/p/e1d8b4eb0ecd). All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
